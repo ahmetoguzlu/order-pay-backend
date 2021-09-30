3945c468b43e9376af10e07941b503aa50c6f5d1
ahmetoguzlu <ahmetoguzlu@gmail.com>
2021-09-29 22:39:24 -0700

    updated serializers to include pk as a field

--- menu/serializers.py
+++ menu/serializers.py
@@ -4,9 +4,9 @@ from menu.models import Section, Item
 class SectionSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Section
-		fields = ['name', 'img', 'order_num']
+		fields = ['name', 'img', 'order_num', 'pk']
 
 class ItemSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Item
-		fields = ['section', 'name', 'img', 'description', 'price', 'options_binary', 'options_selection']
+		fields = ['section', 'name', 'img', 'description', 'price', 'options_binary', 'options_selection', 'pk']
